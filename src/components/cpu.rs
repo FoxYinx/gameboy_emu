106554ec5cd0a18381b3b8b8bff8c54178e154e1
yinx <tollemerkevin@gmail.com>
2025-03-09 18:38:46 +0100

    Added opcode 0x25

--- src/components/cpu.rs
+++ src/components/cpu.rs
@@ -322,6 +322,20 @@ impl Cpu {
                 self.registers.set_h((original & 0x0F) == 0x0F);
                 false
             }
+            0x25 => {
+                self.cycles = self.cycles.wrapping_add(4);
+                let original = self.registers.h;
+                self.registers.h = self.registers.h.wrapping_sub(1);
+
+                if self.debug_instructions {
+                    println!("Opcode: {:#04X} DEC H, H now is {:#04X}, at PC {:#06X}", opcode, self.registers.h, self.registers.pc);
+                }
+
+                self.registers.set_z(self.registers.h == 0);
+                self.registers.set_n(true);
+                self.registers.set_h((original & 0x0F) == 0x00);
+                false
+            }
             0x26 => {
                 self.cycles = self.cycles.wrapping_add(8);
                 self.registers.pc = self.registers.pc.wrapping_add(1);
