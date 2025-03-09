088c873a3bafedc87dba8902506f67e677307a50
yinx <tollemerkevin@gmail.com>
2025-03-09 12:16:52 +0100

    Added opcode 0x05

--- src/components/cpu.rs
+++ src/components/cpu.rs
@@ -82,6 +82,20 @@ impl Cpu {
 
                 false
             }
+            0x05 => {
+                self.cycles = self.cycles.wrapping_add(4);
+                let original = self.registers.b;
+                self.registers.b = self.registers.b.wrapping_sub(1);
+
+                if self.debug_instructions {
+                    println!("Opcode: {:#04X} DEC B, B now is {:#04X}, at PC {:#06X}", opcode, self.registers.b, self.registers.pc);
+                }
+
+                self.registers.set_z(self.registers.b == 0);
+                self.registers.set_n(true);
+                self.registers.set_h((original & 0x0F) == 0x00);
+                false
+            }
             0x06 => {
                 self.cycles = self.cycles.wrapping_add(8);
                 self.registers.pc = self.registers.pc.wrapping_add(1);
