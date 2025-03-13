9b29ec4041509849dce95569286faff16ead001c
yinx <tollemerkevin@gmail.com>
2025-03-13 22:58:30 +0100

    Added 5th test

--- src/components/gameboy.rs
+++ src/components/gameboy.rs
@@ -120,4 +120,13 @@ mod tests {
         let output = gameboy.memory.get_serial_output().get_output();
         assert!(output.contains("Passed"), "Test failed. Output: {}", output);
     }
+
+    #[test]
+    fn rom_05_op_rp() {
+        let mut gameboy = Gameboy::new();
+        gameboy.cartridge_to_rom(String::from("resources/roms/cpu_instrs/individual/05-op rp.gb"));
+        gameboy.start(Some(2_000_000));
+        let output = gameboy.memory.get_serial_output().get_output();
+        assert!(output.contains("Passed"), "Test failed. Output: {}", output);
+    }
 }
