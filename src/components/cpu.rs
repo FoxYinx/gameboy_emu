4a9f1bd761e7417b4f5ea4a254b70f70facf78eb
yinx <tollemerkevin@gmail.com>
2025-03-14 14:50:21 +0100

    Added opcode 0xD4

--- src/components/cpu.rs
+++ src/components/cpu.rs
@@ -2046,6 +2046,45 @@ impl Cpu {
                     (false, 12)
                 }
             }
+            0xD3 => {
+                panic!("This opcode: {:#04X} doesn't exist, at PC {:#06X}", opcode, self.registers.pc);
+            }
+            0xD4 => {
+                if !self.registers.get_c() {
+                    self.registers.pc = self.registers.pc.wrapping_add(1);
+                    if let Some(low) = memory.get(self.registers.pc as usize) {
+                        self.registers.pc = self.registers.pc.wrapping_add(1);
+                        if let Some(high) = memory.get(self.registers.pc as usize) {
+                            let address = ((*high as u16) << 8) | *low as u16;
+                            let return_address = self.registers.pc.wrapping_add(1);
+                            self.registers.sp = self.registers.sp.wrapping_sub(1);
+                            memory.write_memory(self.registers.sp as usize, (return_address >> 8) as u8);
+                            self.registers.sp = self.registers.sp.wrapping_sub(1);
+                            memory.write_memory(self.registers.sp as usize, return_address as u8);
+
+                            if self.debug_instructions {
+                                println!("Opcode: {:#04X} CALL NC a16, with a16 = {:#06X}, at PC {:#06X}", opcode, address, self.registers.pc.wrapping_sub(2));
+                            }
+
+                            self.registers.pc = address;
+                            (true, 24)
+                        } else {
+                            eprintln!("Failed to get high value of call address at PC {:#06X}", self.registers.pc);
+                            (false, 24)
+                        }
+                    } else {
+                        eprintln!("Failed to get low value of call address at PC {:#06X}", self.registers.pc);
+                        (false, 24)
+                    }
+                } else {
+                    self.registers.pc = self.registers.pc.wrapping_add(2);
+
+                    if self.debug_instructions {
+                        println!("Opcode: {:#04X} CALL NC a16 but Z was true", opcode);
+                    }
+                    (false, 12)
+                }
+            }
             0xD5 => {
                 let de = self.registers.get_de();
                 let low = de as u8;
