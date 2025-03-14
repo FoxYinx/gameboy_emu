94eea3f81f6421d9adae563dd2aa9ee8615c50b6
yinx <tollemerkevin@gmail.com>
2025-03-14 21:57:02 +0100

    Added all ADC opcodes

--- src/components/cpu.rs
+++ src/components/cpu.rs
@@ -2255,6 +2255,201 @@ impl Cpu {
 
                 (false, 4)
             }
+            0x88 => {
+                let carry = self.registers.get_c() as u8;
+                let result = self.registers.a.wrapping_add(self.registers.b).wrapping_add(carry);
+                self.registers.set_z(result == 0);
+                self.registers.set_n(false);
+                let h_check = (self.registers.a & 0x0F) + (self.registers.b & 0x0F) + carry;
+                self.registers.set_h(h_check > 0x0F);
+                let c_check = (self.registers.a as u16) + (self.registers.b as u16) + (carry as u16);
+                self.registers.set_c(c_check > 0xFF);
+                self.registers.a = result;
+
+                if self.debug_instructions {
+                    println!(
+                        "Opcode: {:#04X} ADC A B, with A = {:#04X} & B = {:#04X} & Ca = {}, at PC {:#06X}",
+                        opcode,
+                        self.registers.a,
+                        self.registers.b,
+                        carry,
+                        self.registers.pc
+                    );
+                }
+
+                (false, 4)
+            }
+            0x89 => {
+                let carry = self.registers.get_c() as u8;
+                let result = self.registers.a.wrapping_add(self.registers.c).wrapping_add(carry);
+                self.registers.set_z(result == 0);
+                self.registers.set_n(false);
+                let h_check = (self.registers.a & 0x0F) + (self.registers.c & 0x0F) + carry;
+                self.registers.set_h(h_check > 0x0F);
+                let c_check = (self.registers.a as u16) + (self.registers.c as u16) + (carry as u16);
+                self.registers.set_c(c_check > 0xFF);
+                self.registers.a = result;
+
+                if self.debug_instructions {
+                    println!(
+                        "Opcode: {:#04X} ADC A C, with A = {:#04X} & C = {:#04X} & Ca = {}, at PC {:#06X}",
+                        opcode,
+                        self.registers.a,
+                        self.registers.c,
+                        carry,
+                        self.registers.pc
+                    );
+                }
+
+                (false, 4)
+            }
+            0x8A => {
+                let carry = self.registers.get_c() as u8;
+                let result = self.registers.a.wrapping_add(self.registers.d).wrapping_add(carry);
+                self.registers.set_z(result == 0);
+                self.registers.set_n(false);
+                let h_check = (self.registers.a & 0x0F) + (self.registers.d & 0x0F) + carry;
+                self.registers.set_h(h_check > 0x0F);
+                let c_check = (self.registers.a as u16) + (self.registers.d as u16) + (carry as u16);
+                self.registers.set_c(c_check > 0xFF);
+                self.registers.a = result;
+
+                if self.debug_instructions {
+                    println!(
+                        "Opcode: {:#04X} ADC A D, with A = {:#04X} & D = {:#04X} & Ca = {}, at PC {:#06X}",
+                        opcode,
+                        self.registers.a,
+                        self.registers.d,
+                        carry,
+                        self.registers.pc
+                    );
+                }
+
+                (false, 4)
+            }
+            0x8B => {
+                let carry = self.registers.get_c() as u8;
+                let result = self.registers.a.wrapping_add(self.registers.e).wrapping_add(carry);
+                self.registers.set_z(result == 0);
+                self.registers.set_n(false);
+                let h_check = (self.registers.a & 0x0F) + (self.registers.e & 0x0F) + carry;
+                self.registers.set_h(h_check > 0x0F);
+                let c_check = (self.registers.a as u16) + (self.registers.e as u16) + (carry as u16);
+                self.registers.set_c(c_check > 0xFF);
+                self.registers.a = result;
+
+                if self.debug_instructions {
+                    println!(
+                        "Opcode: {:#04X} ADC A E, with A = {:#04X} & E = {:#04X} & Ca = {}, at PC {:#06X}",
+                        opcode,
+                        self.registers.a,
+                        self.registers.e,
+                        carry,
+                        self.registers.pc
+                    );
+                }
+
+                (false, 4)
+            }
+            0x8C => {
+                let carry = self.registers.get_c() as u8;
+                let result = self.registers.a.wrapping_add(self.registers.h).wrapping_add(carry);
+                self.registers.set_z(result == 0);
+                self.registers.set_n(false);
+                let h_check = (self.registers.a & 0x0F) + (self.registers.h & 0x0F) + carry;
+                self.registers.set_h(h_check > 0x0F);
+                let c_check = (self.registers.a as u16) + (self.registers.h as u16) + (carry as u16);
+                self.registers.set_c(c_check > 0xFF);
+                self.registers.a = result;
+
+                if self.debug_instructions {
+                    println!(
+                        "Opcode: {:#04X} ADC A H, with A = {:#04X} & H = {:#04X} & Ca = {}, at PC {:#06X}",
+                        opcode,
+                        self.registers.a,
+                        self.registers.h,
+                        carry,
+                        self.registers.pc
+                    );
+                }
+
+                (false, 4)
+            }
+            0x8D => {
+                let carry = self.registers.get_c() as u8;
+                let result = self.registers.a.wrapping_add(self.registers.l).wrapping_add(carry);
+                self.registers.set_z(result == 0);
+                self.registers.set_n(false);
+                let h_check = (self.registers.a & 0x0F) + (self.registers.l & 0x0F) + carry;
+                self.registers.set_h(h_check > 0x0F);
+                let c_check = (self.registers.a as u16) + (self.registers.l as u16) + (carry as u16);
+                self.registers.set_c(c_check > 0xFF);
+                self.registers.a = result;
+
+                if self.debug_instructions {
+                    println!(
+                        "Opcode: {:#04X} ADC A L, with A = {:#04X} & L = {:#04X} & Ca = {}, at PC {:#06X}",
+                        opcode,
+                        self.registers.a,
+                        self.registers.l,
+                        carry,
+                        self.registers.pc
+                    );
+                }
+
+                (false, 4)
+            }
+            0x8E => {
+                if let Some(value) = memory.get(self.registers.get_hl() as usize) {
+                    let carry = self.registers.get_c() as u8;
+                    let result = self.registers.a.wrapping_add(*value).wrapping_add(carry);
+                    self.registers.set_z(result == 0);
+                    self.registers.set_n(false);
+                    let h_check = (self.registers.a & 0x0F) + (*value & 0x0F) + carry;
+                    self.registers.set_h(h_check > 0x0F);
+                    let c_check = (self.registers.a as u16) + (*value as u16) + (carry as u16);
+                    self.registers.set_c(c_check > 0xFF);
+                    self.registers.a = result;
+
+                    if self.debug_instructions {
+                        println!(
+                            "Opcode: {:#04X} ADC A [HL], with A = {:#04X} & [HL] = {:#04X} & Ca = {}, at PC {:#06X}",
+                            opcode,
+                            self.registers.a,
+                            *value,
+                            carry,
+                            self.registers.pc
+                        );
+                    }
+                } else {
+                    eprintln!("Failed to get value at HL {:#06X}", self.registers.get_hl());
+                }
+                
+                (false, 4)
+            }
+            0x8F => {
+                let carry = self.registers.get_c() as u8;
+                let result = self.registers.a.wrapping_add(self.registers.a).wrapping_add(carry);
+                self.registers.set_z(result == 0);
+                self.registers.set_n(false);
+                let h_check = (self.registers.a & 0x0F) + (self.registers.a & 0x0F) + carry;
+                self.registers.set_h(h_check > 0x0F);
+                let c_check = (self.registers.a as u16) + (self.registers.a as u16) + (carry as u16);
+                self.registers.set_c(c_check > 0xFF);
+                self.registers.a = result;
+
+                if self.debug_instructions {
+                    println!(
+                        "Opcode: {:#04X} ADC A A, with A = {:#04X} & Ca = {}, at PC {:#06X}",
+                        opcode,
+                        self.registers.a,
+                        carry,
+                        self.registers.pc
+                    );
+                }
+
+                (false, 4)
+            }
             0xA8 => {
                 if self.debug_instructions {
                     println!(
