bddb1e0aa4dfee6e78c1151d44040b38bc2d327a
yinx <tollemerkevin@gmail.com>
2025-03-14 23:26:58 +0100

    Removed useless timer

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,13 @@ mod io;
 mod utils;
 
 use components::gameboy::Gameboy;
-use std::time::Instant;
 
 fn main() {
-    let start = Instant::now();
-
     let mut gameboy = Gameboy::new();
     //gameboy.toggle_debug_instructions();
     //gameboy.toggle_debug_registers();
     gameboy.cartridge_to_rom(String::from(
-        "resources/roms/cpu_instrs/individual/11-op a,(hl).gb",
+        "resources/roms/cpu_instrs/cpu_instrs.gb",
     ));
     gameboy.start(None);
-
-    let end = Instant::now();
-    println!("Time: {:?}", end.duration_since(start));
 }
